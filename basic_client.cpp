07ef865762bf3c2763647da2615800150181597a
elvisxiaozhi <elvisxiaozhi@gmail.com>
2019-04-08 17:00:39 +0800

    add basic_clone.cpp

--- basic_client.cpp
+++ basic_client.cpp
@@ -12,11 +12,15 @@ using std::cin;
 
 int main()
 {
-	int sock, strLen;
-	char message[255];
+	int sock, readLen = 0, index = 0;
+	char message[30];
 	struct sockaddr_in serverAddr;
 
 	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock == -1) {
+		cout << "sock error" << endl;
+	}
+
 	memset(&serverAddr, 0, sizeof(serverAddr));
 
 	string port;
@@ -27,11 +31,22 @@ int main()
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serverAddr.sin_port = htons(atoi(port.c_str()));
 
-	connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	if (connect(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+		cout << "connect error" << endl;
+	}
 
-	strLen = read(sock, message, 255);
+	int strLen = 0;
+	while ((readLen = read(sock, &message[++index], 1))) {
+		if (readLen == -1) {
+			cout << "read error" << endl;
+		}
+
+		strLen += readLen;
+	};
+
 	cout << "Message from server: " << message << endl;
-	
+	cout << "read call count: " << strLen << endl;
+
 	close(sock);
 
 	return 0;
